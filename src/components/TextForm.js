187054f4e5e344fc199ab3733ccfc41782d1a431
Garv Agarwal <garv51104@gmail.com>
2024-06-02 23:20:33 +0530

    Improving Responsiveness

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,18 +37,18 @@ export default function TextForm(props) {
 
   return (
     <>
-      <div className="container my-3">
-          <h1 className={`text-${props.mode==='dark'?'light':'dark'}`}>{props.heading}</h1>
+      <div className="sm-container my-3">
+          <h1 className={`my-sm-3 my-4 text-${props.mode==='dark'?'light':'dark'}`}>{props.heading}</h1>
         <div className="mb-3">
           <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#dedede':'white'}} id="myBox" rows="5" ></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to UPPERCASE</button>
-        <button className="btn btn-success mx-3" onClick={handleLowClick}>Convert to lowercase</button>
-        <button className="btn btn-warning btn-md my-lg-0 my-sm-3" onClick={handleCopy}>Copy Text</button>
-        <button className="btn btn-danger mx-3" onClick={handleTextDelete}>Clear Text</button>
+        <button className="btn btn-success mx-sm-3 my-sm-0 my-2" onClick={handleLowClick}>Convert to lowercase</button>
+        <button className="btn btn-warning mx-sm-0 mx-2" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-danger mx-sm-3" onClick={handleTextDelete}>Clear Text</button>
         {/* <button className="btn btn-danger md-my-3" onClick={handleMultiply}>Multiply me</button> */}
       </div>
-      <div className={`container my-3 text-${props.mode==='dark'?'light':'dark'}`}>
+      <div className={`my-3 text-${props.mode==='dark'?'light':'dark'}`}>
           <h2 className={`text-${props.mode==='dark'?'light':'dark'}`}>Your text summary here</h2>
           <p>{text.split(" ").length} words, {text.length} characters</p>
       </div>
